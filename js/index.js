658647cbf33282a1f6c6483be8232adaf042f861
wngml02 <wngml021211@gmail.com>
2022-12-12 05:37:04 +0900

    edit

--- js/index.js
+++ js/index.js
@@ -7,18 +7,6 @@ function change(){
   body.style.backgroundImage = "url('static/orangeblue.jpg')";
 }
 
-function init() {
-  var tbody;
-  tbody = document.getElementById('a');
-  tbody.addEventListener("mouseover", over);
-  tbody.addEventListener("mouseout",out);
-}
-function over(){
-  tbody.style.backgroundColor = "lightcoral";
-}
-function out(){
-  tbody.style.backgroundColor = "antiquewhite";
-}
 function siteload() {
 	let select = document.getElementById("site");
 	window.location=select.options[select.selectedIndex].value;
